0d00a45f6b1c174f88d1f144ab120ddbd426dd96
usheth <utsavsheth1994@gmail.com>
2019-06-28 02:15:53 -0700

    Done

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -47,9 +47,11 @@ Theta_grad = zeros(size(Theta));
 
 
 
-J = (1/2)*sum(sum(R.*((Theta*X')' - Y).^2))
-%J = 1/2 * sum(sum((R.* ((X*Theta') - Y)).^2));
+J = (1/2)*sum(sum(R.*((Theta*X')' - Y).^2)) + (lambda/2)*(sum(Theta(:).^2) + sum(X(:).^2))
 
+
+X_grad = (R .* (X*Theta' - Y)) * Theta + lambda*X;
+Theta_grad = (R .* (X*Theta' - Y))' * X + lambda*Theta;
 
 
 
